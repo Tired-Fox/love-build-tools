c2fd7111f660dfbff8ec3d91a703ab0128777aba
Tired-Fox <zboehm104@gmail.com>
2024-09-28 23:05:33 -0500

    [feat] strip out llam

--- src/git/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod client;
-pub mod cli;
-
-pub use client::Client;
-pub use cli::Cli;
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,3 @@
-use clap::Parser;
-
-use llam::{Addon, Error, Manager};
-
-/// Lua Language Addon Manager
-/// 
-/// Used to install and manage lua language server addons. The idea being that it installs them to a set location
-/// then adds a `.luarc.json` file to the current location to expose the addons.
-#[derive(Debug, Parser)]
-#[command(name = "llam", version, about, long_about = None)]
-struct LLAM {
-    #[command(subcommand)]
-    command: Subcommand,     
-}
-
-#[derive(Debug, clap::Subcommand)]
-enum Subcommand {
-    Add {
-        addons: Vec<Addon>
-    }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let mut manager = Manager::new("examples/blocks", "love-build-tools")?;
-
-    let llam = LLAM::parse();
-
-    match llam.command {
-        Subcommand::Add { addons } => {
-            manager.add(addons)?;
-        }
-    }
-
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }
